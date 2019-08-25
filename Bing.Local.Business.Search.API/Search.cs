19fea8a3ee39a59b1af70f630afea2ead984b30b
Anouar <anouar.ben.zahra@hotmail.com>
2019-08-25 19:55:11 +0200

    first ADd

---
+++ Bing.Local.Business.Search.API/Search.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bing.Local.Business.Search.API
+{
+    public class Search
+    {
+        //const string accessKey = "enter key here";
+
+        const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/localbusinesses/search";
+
+        //const string searchTerm = "restaurant in Bellevue";
+
+        // Returns search results including relevant headers
+        struct SearchResult
+        {
+            public String jsonResult;
+            public Dictionary<String, String> relevantHeaders;
+        }
+
+        static SearchResult Get(string key, string inputText)
+        {
+           
+            var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(inputText) + "&mkt=en-us";
+
+            // Perform the Web request and get the response
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = key;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // Create result object for return
+            var searchResult = new SearchResult();
+            searchResult.jsonResult = json;
+            searchResult.relevantHeaders = new Dictionary<String, String>();
+
+            // Extract Bing HTTP headers
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+
+            return searchResult;
+        }
+    }
+}
